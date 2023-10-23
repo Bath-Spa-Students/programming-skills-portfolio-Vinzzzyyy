ac3ab02303937a0ff635c1865cbacc39f6ee00d1
Vinzzzyyy <patrickmeriales@gmail.com>
2023-10-22 22:25:25 -1100

    exercise2.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise2.py
@@ -0,0 +1,2 @@
+print ('Martin Luther King once said, "I have a dream that my four little children will one day live in a')
+print ('nation where they will not be judged by the color of their skin but by the content of their character."')
