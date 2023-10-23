e159c316a5dfc531f33e1e759293c133b29dfc67
Vinzzzyyy <patrickmeriales@gmail.com>
2023-10-22 23:10:49 -1100

    exercise3.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise3.py
@@ -0,0 +1,7 @@
+transportation = ['Nmax', 'Aerox', 'Raider 150']
+name = transportation[0]
+print(f"I would like to drive a {name}.")
+name = transportation[1]
+print(f"I drive an {name} in the Philippines")
+name = transportation[2]
+print(f"My cousin drives an {name}")
