de231fb772f662fb6046b021366a442d6bf9041d
Vinzzzyyy <patrickmeriales@gmail.com>
2023-10-22 22:37:09 -1100

    exercise3.py

---
+++ Chapter 2- Variables & Comments/Exercises/Exercise3.py
@@ -0,0 +1,17 @@
+name ="\tVinz Patrick Meriales\n"
+
+
+print("Unmodified:")
+print(name)
+
+
+print("\nUsing lstrip():")
+print(name.lstrip())
+
+
+print("\nUsing rstrip():")
+print(name.rstrip())
+
+
+print("\nUsing strip():")
+print(name.strip())
