9ef0cdc176103b118b9c6b28568d9aab55d7369b
Vinzzzyyy <patrickmeriales@gmail.com>
2023-10-22 23:56:04 -1100

    Exercise6.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise6.py
@@ -0,0 +1,22 @@
+guests= ['Ailene', 'Arianne', 'Xander']
+name = guests[0]
+print(f"{name}, please come down to eat dinner")
+name = guests[1]
+print(f"{name}, please come down to eat dinner")
+name = guests[2]
+print(f"{name}, please come down to eat dinner")
+
+
+print("\nSorry, we can only invite two people to dinner.")
+name = guests.pop
+print(f"Sorry, {guests[2]} there's no room at the table.")
+
+
+name = guests[0]
+print(f"{name}, please come down to eat dinner.")
+name = guests[1]
+print(f"{name}, please come down to eat dinner.")
+del(guests[0])
+del(guests[0])
+del(guests[0])
+print(guests)
