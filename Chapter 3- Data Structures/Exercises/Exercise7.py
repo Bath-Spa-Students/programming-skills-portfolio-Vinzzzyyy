f7f8c7a0bad9b04a105a2d5bba51b50e166e420d
Vinzzzyyy <patrickmeriales@gmail.com>
2023-10-23 00:08:09 -1100

    Exercise7.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise7.py
@@ -0,0 +1,31 @@
+locations = ['Europe', 'America', 'Japan', 'Korea', 'Hawai']
+print("Original order:")
+print(locations)
+
+print("\nAlphabetical:")
+print(sorted(locations))
+
+print("\nOriginal order:")
+print(locations)
+
+print("\nReverse alphabetical:")
+print(sorted(locations, reverse=True))
+
+print("\nOriginal order:")
+print(locations)
+
+print("\nReversed:")
+locations.reverse()
+print(locations)
+
+print("\nOrignal order:")
+locations.reverse()
+print(locations)
+
+print("\nAlphabetical")
+locations.sort()
+print(locations)
+
+print("\nReverse alphabetical")
+locations.sort(reverse=True)
+print(locations)
