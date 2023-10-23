4bf0aebde576d72b493c389979534a51238862b7
Vinzzzyyy <patrickmeriales@gmail.com>
2023-10-22 23:00:50 -1100

    exercise2.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise2.py
@@ -0,0 +1,10 @@
+names = ['joshua', 'jhan joe', 'darryl', 'ceazar']
+
+msg = f"Hi, {names[0]}!"
+print(msg)
+msg = f"Hi, {names[1]}!"
+print(msg)
+msg = f"Hi, {names[2]}!"
+print(msg)
+msg = f"Hi, {names[3]}!"
+print(msg)
