5c6964df2a1570f7dde7aa8fa369f77b0b6bdeb3
Vinzzzyyy <patrickmeriales@gmail.com>
2023-10-22 23:21:16 -1100

    Exercise5.py

---
+++ Chapter 3- Data Structures/Exercises/Exercise5.py
@@ -0,0 +1,21 @@
+guests= ['Ailene', 'Arianne', 'Richard']
+name = guests[0]
+print(f"{name}, please come down to eat dinner")
+name = guests[1]
+print(f"{name}, please come down to eat dinner")
+name = guests[2]
+print(f"{name}, please come down to eat dinner")
+
+
+name = guests [2]
+print(f"\nSorry, {name} can't make it to dinner")
+
+
+
+guests= ['Ailene', 'Arianne', 'Xander']
+name = guests[0]
+print(f"{name}, please come down to eat dinner")
+name = guests[1]
+print(f"{name}, please come down to eat dinner")
+name = guests[2]
+print(f"{name}, please come down to eat dinner")
