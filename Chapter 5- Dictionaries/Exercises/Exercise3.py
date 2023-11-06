9d6a7b763269705a1338f678655274f1ece6e175
Vinzzzyyy <patrickmeriales@gmail.com>
2023-11-05 22:32:21 -1100

    Exercise3.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise3.py
@@ -0,0 +1,41 @@
+Glossary = {
+    'string': 'A series of characters.',
+    'float': 'A numerical value with a decimal component.',
+    'list': 'A collection of items in a particular order.',
+    'value': 'An item associated with a key in a dictionary',
+    'dictionary': 'A collection of key-value pairs.',
+    'function': 'group of statements within a program that perform a specific task',
+    'Boolean Expression': 'An expression that evaluates to True or False',
+    'index': 'A number specifying the postion of an element in a list',
+    'concatenate': 'joining two things together',
+    'append': 'used to add items to a list - item is appended to the end of the existing list',
+}
+word = 'string'
+print("\n" + word.title () + ":" + Glossary[word])
+
+word = 'float'
+print("\n" + word.title () + ":" + Glossary[word])
+
+word = 'list'
+print("\n" + word.title () + ":" + Glossary[word])
+
+word = 'value'
+print("\n" + word.title () + ":" + Glossary[word])
+
+word = 'dictionary'
+print("\n" + word.title () + ":" + Glossary[word])
+
+word = 'function'
+print("\n" + word.title () + ":" + Glossary[word])
+
+word = 'Boolean Expression'
+print("\n" + word.title () + ":" + Glossary[word])
+
+word = 'index'
+print("\n" + word.title () + ":" + Glossary[word])
+
+word = 'concatenate'
+print("\n" + word.title () + ":" + Glossary[word])
+
+word = 'append'
+print("\n" + word.title () + ":" + Glossary[word])
