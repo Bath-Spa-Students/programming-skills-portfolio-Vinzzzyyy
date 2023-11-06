2cb59cdd1126c59c8e094b5b418d96b05f3bd28f
Vinzzzyyy <patrickmeriales@gmail.com>
2023-11-05 22:32:12 -1100

    Exercise2.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise2.py
@@ -0,0 +1,22 @@
+Glossary = {
+    'string': 'A series of characters.',
+    'float': 'A numerical value with a decimal component.',
+    'list': 'A collection of items in a particular order.',
+    'value': 'An item associated with a key in a dictionary',
+    'dictionary': 'A collection of key-value pairs.',
+}
+
+word = 'string'
+print("\n" + word.title () + ":" + Glossary[word])
+
+word = 'float'
+print("\n" + word.title () + ":" + Glossary[word])
+
+word = 'list'
+print("\n" + word.title () + ":" + Glossary[word])
+
+word = 'value'
+print("\n" + word.title () + ":" + Glossary[word])
+
+word = 'dictionary'
+print("\n" + word.title () + ":" + Glossary[word])
