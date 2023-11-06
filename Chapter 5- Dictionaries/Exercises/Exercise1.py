46b1a3f701e00ed4e323452bcea85ea21ac7d119
Vinzzzyyy <patrickmeriales@gmail.com>
2023-11-05 22:32:02 -1100

    Exercise1.py

---
+++ Chapter 5- Dictionaries/Exercises/Exercise1.py
@@ -0,0 +1,10 @@
+person = {
+    'first name' : 'Darryl',
+    'last name' : 'Jones', 
+    'age' : 25,
+    'city' : 'Michigan',
+}
+print(person['first name'])
+print(person['last name'])
+print(person['age'])
+print(person['city'])
