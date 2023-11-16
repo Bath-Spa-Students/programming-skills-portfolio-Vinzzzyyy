c23585901b57545d8cfa029842a4b893abc0948e
Vinzzzyyy <patrickmeriales@gmail.com>
2023-11-16 10:12:11 -1100

    Exercise3.py

---
+++ Chapter 7- Functions/Exercises/Exercise3.py
@@ -0,0 +1,7 @@
+def make_shirt(size, message):
+    """Summarize the shirt that's going to be made."""
+    print("\nI'm going to create a " + size + " t-shirt.")
+    print('It will say, "' + message +'"')
+
+make_shirt('x-large' , 'The wind is always by my side')
+make_shirt(message="Shadow-Realms." ,  size='small')
